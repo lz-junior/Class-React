c314733b40169c4492b51d6dec6125203ff77e08
lz-junior <correajr80@hotmail.com>
2023-05-26 23:05:07 -0300

    class fifteen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Testanto Eventos</h1>
-
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/empresa">Empresa</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/empresa">
+          <Empresa />
+        </Route>
+        <Route path="/contato">
+          <Contato />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
